aee2e3f823b3ffdb9721f16003b544a6fd9b405c
Dmigdol <Dillonmigdol@gmail.com>
2024-02-05 18:06:26 -0800

    Set up basic routing, Next step is to render App.js

---
+++ server/controllers/test.js
@@ -0,0 +1,17 @@
+const models = require('../models/test');
+
+module.exports = {
+  getCountries(req, res) {
+    console.log('******REQ PARAMS********', req.params);
+    const countries = req.params.countries;
+    models.getCountries()
+      .then((results) => {
+        console.log('*****RESULTS******', results);
+        res.send(results);
+      });
+  }
+
+
+
+
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import axios from 'axios';
 
-const supabase = createClient("https://lgfhzumbxexugypmmnew.supabase.co/",
- "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnZmh6dW1ieGV4dWd5cG1tbmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxNzI2NTQsImV4cCI6MjAyMjc0ODY1NH0.zCHDum90o-4d6OkehiIqIhfgoZzuSL9VNBDGGPXdJ30");
 
 
 function App() {
-
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
 
   return (
     <div className="App">
@@ -35,11 +22,26 @@ function App() {
         >
           Learn React
         </a>
-        <ul>
-        {countries.map((country) => (
-          <li key={country.name}>{country.name}</li>
-        ))}
-      </ul>
+        <div>
+          <button
+          onClick={() => {
+            axios.get(`http://localhost:5000/test`)
+              .then(function (response) {
+                //handle success
+                console.log(response);
+              })
+              .catch(function (error) {
+                //handle error
+                console.log('***ERROR*** ',error)
+              })
+              .finally(() => {
+                console.log('request finished')
+              });
+          }}
+          >
+            Test
+          </button>
+        </div>
       </header>
     </div>
   );

a69f1ea7f6aad49f11d0fded9aba5c05fe9b6967
Dmigdol <Dillonmigdol@gmail.com>
2024-02-08 15:23:10 -0800

    Fixed routing, must start nodemon server -> npm server

--- server/index.js
+++ server/index.js
@@ -10,11 +10,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.render('')
-})
+app.use(express.static('build'));
 
 
 app.use('/api', router.test);
--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
@@ -9,23 +7,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> branch test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
           <button
           onClick={() => {
-            axios.get(`http://localhost:5000/test`)
+            axios.get(`http://localhost:3000/api/test`)
               .then(function (response) {
                 //handle success
                 console.log(response);
@@ -41,8 +25,6 @@ function App() {
           >
             Test
           </button>
-        </div>
-      </header>
     </div>
   );
 }

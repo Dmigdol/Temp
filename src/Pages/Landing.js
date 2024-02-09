beeaaa9864d133ac72035dfca7a41d1e87df3a8c
Dmigdol <Dillonmigdol@gmail.com>
2024-02-08 18:23:08 -0800

    Set up first rendition of Landing/Sign in Page

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -4,23 +4,53 @@ import axios from 'axios';
 
 
 
-function App() {
+function Landing() {
+
+  const [email, setEmail] = useState('Email...');
+  const [password, setPassword] = useState('Password...');
 
   return (
     <div className="App">
       <div className="Landing-Frame">
-        <div className="Logo" />
-        <span className="Heading"> Sign In</span>
-        <div className="Email-Entry"></div>
-        <div className="Password-Entry"></div>
-        <button className="Login"></button>
-        <p className="Forgot-Login"></p>
+        <div className="Container Logo">
+          <img className="Logoimg" src='VFlogo.png'/>
+        </div>
+          <div class="Container Header">
+          <span className="Heading"> Sign In</span>
+        </div>
+        <div className="Container input">
+          <form className="email">
+            <input
+            className='input'
+            placeholder='Email...'
+            type='text'
+            onChange={(e) => setEmail(e.target.value)}
+            />
+          </form>
+          <form className="password">
+            <input
+            className='input'
+            placeholder='Password...'
+            type='text'
+            onChange={(e) => setPassword(e.target.value)}
+            />
+          </form>
+        </div>
+        <button class="button login"
+          onClick={()=>{ alert(`Login Clicked
+          Email is ${email}.
+          Password is ${password}.
+          `); }}
+        >Login</button>
+        <div className="forgot"
+          onClick={()=>{ alert('Forgot Login Clicked'); }}
+        >Forgot Login?</div>
       </div>
     </div>
   );
 }
 
-export default App;
+export default Landing;
 
 {/* <button className='test'
           onClick={() => {

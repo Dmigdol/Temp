7fc3564e0af271fb41f1ece12f27533107f51aec
Dmigdol <Dillonmigdol@gmail.com>
2024-02-10 16:36:55 -0800

    Added basic Headbar with Logout button to return to Login screen

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from "react";
-import '../Sass/Login.scss'
+import '../Sass/Landing.scss'
 import axios from 'axios';
 
-function Landing() {
+function Landing({setCurrentPage}) {
 
   return (
     <div className="App">
-      <div className="landing frame"></div>
+      <div className="landing frame">
+        <div className="headbar">
+          <span className="logout"
+          onClick={() => setCurrentPage('Login')}
+          >Logout</span>
+          <div className="Container Landing-Logo">
+          <img className="Logoimg" src='VFlogo.png'/>
+          </div>
+          <div class="Container Landing-Header">
+            <span className="Landing-Heading">Welcome Back!</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

cbcbb7553451db5da38d3e10dfd61235cd1f02f6
Dmigdol <Dillonmigdol@gmail.com>
2024-02-27 14:58:32 -0800

    Added skeleton for QuoteBuilder

--- src/Pages/QuoteBuilder.jsx
+++ src/Pages/QuoteBuilder.jsx
@@ -3,8 +3,17 @@ import { useEffect, useState } from "react";
 function QuoteBuilder({setCurrentPage}) {
 
   return(
-    <div>
-      Hello World
+    <div className='quote container'>
+      <div className='quote-info'>
+        <span className='quote-info-text'></span>
+      </div>
+      <div className='rows-container'>
+        <div className='rows header'>
+          {/* Each Row Rendered Here as its own div*/}
+        </div>
+      </div>
+      <div className='NewRow button'>
+      </div>
     </div>
   )
 }

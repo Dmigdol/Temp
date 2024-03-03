73b1165f409b4647f171dab543f2a1e23552abc5
Dmigdol <Dillonmigdol@gmail.com>
2024-03-03 11:24:26 -0800

    Completed basic skeleton for new row

--- src/Pages/NewRow.jsx
+++ src/Pages/NewRow.jsx
@@ -2,9 +2,28 @@ import '../NewRow.scss'
 
 function newRow() {
 
+
   return (
 
-    <div className=''></div>
+    <div className='Entry-frame'>
+      <div className='Input-container'>
+        <div className='Label input'>
+        </div>
+        <div className='Height input'>
+        </div>
+        <div className='Weight input'>
+        </div>
+      </div>
+      <div className='Drop-container'>
+        <div className='DoorFrame selection'>
+        </div>
+        <div className='Checkbox'>
+          {/* single or double? */}
+        </div>
+        <div className='Hinge selection'>
+        </div>
+      </div>
+    </div>
   )
 }
 

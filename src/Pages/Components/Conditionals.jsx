abfdf98cb9635ffffb0564dc187428c34e1d64ac
Dmigdol <Dillonmigdol@gmail.com>
2024-05-07 16:39:26 -0700

    Added Conditional page to modal and the logic for height comparing to standard frames

--- src/Pages/Components/Conditionals.jsx
+++ src/Pages/Components/Conditionals.jsx
@@ -1,69 +1,68 @@
 import './Conditionals.scss'
 import Select from 'react-select'
 import { useRef, useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 
-function Conditionals({data}) {
+function Conditionals({data, setInputs}) {
 
-  /*
-    Data will be recieved in following format
-      Data {
-        Frame: 'Standard,In-Swing,Pivot',
-        Height: #,
-        NumOfDoors: Single/Double,
-      }
+  console.log('conditionals payload', data)
 
-    Based on this information:
-      return specific drop downs
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setInputs({...data, [name]: value})
+  }
 
-      standard frame ?
-        <72 ? -> 3 options dd
-        72 - 84 ? -> 3 options dd
-        84 - 96 ? -> 3 options dd
-        96 - 120 ? -> 3 options dd
-  */
-
-
-    const frameOptions = [
-      {value: 'standard', label: 'Standard'},
-      {value: 'ins', label: 'In-Swing'},
-      {value: 'pivot', label: 'Pivot'}
-    ]
-
-    // const standardFrame = {
-    //   single: {
-    //     height: `36" up`,
-    //     strike: {
-    //       type: {
-    //         default: [`Lip 2 1/4" Radius`, `Lip 2 1/4" Square`, `T-Strike 1 1/8" x 2 3/4"`,
-    //                     `ASA-Strike 1 1/4" x 4 7/8"`, `No Strike`],
-    //         deadbolt: [`1" x 2 1/4" Radius`, `1" x 2 1/4" Square`, `1 1/8" x 2 3/4" Radius`, `1 1/8" x 2 3/4" Square`]
-    //       },
-    //     },
-    //     hinge: {
-    //       '<72' : [`(2)CalRoyal CR3D51`, `(2)Tectus 340 3D`, `(2) 4x4 butt hinge`],
-    //       '72-84' : [`(3)CalRoyal CR3D51`, `(3)Tectus 340 3D`, `(3) 4x4 butt hinge`],
-    //       '84-96' : [`(3)CalRoyal CR3D62`, `(3)Tectus 540 3D`, `(4) 4x4 butt hinge`],
-    //       '>96' :  [`(4)CalRoyal CR3D62`, `(4)Tectus 540 3D`, `(4) 4x4 butt hinge`]
-    //     }
-    //   },
-    //   double : 'etc...'
-    // }
-
-    const calcDD = () => {
-      // data.height < 72 ? standardFrame[rowObj] :
+  if (data.frame === 'standard') {
+    if(data.height < 72){
+      return(
+        <>
+          <Form.Select name='hinge' onChange={handleInputChange}>
+            <option value='CR3D51-2'>CalRoyal CR3D51 {`(2)`}</option>
+            <option value='340 3D-2'>Tectus 340 3D {`(2)`}</option>
+            <option value='butt-2'>4x4 butt hinge {`(2)`}</option>
+          </Form.Select>
+        </>
+      )
     }
-
-
-  return (
-    <label className='frame-id'>
-      <span className='frame-id-text'>
-      {`Frame Type: `}
-      </span>
-        <Select
-        className='frame-dropdown'
-        options={frameOptions} />
-    </label>
-  )
+  }
+    if(data.height > 72 && data.height < 84){
+      return(
+        <>
+          <Form.Select name='hinge' onChange={handleInputChange}>
+            <option value='CR3D51-3'>CalRoyal CR3D51 {`(3)`}</option>
+            <option value='340 3D-3'>Tectus 340 3D {`(3)`}</option>
+            <option value='butt-3'>4x4 butt hinge {`(3)`}</option>
+          </Form.Select>
+        </>
+      )
+    }
+    if(data.height > 84 && data.height <= 96){
+      return(
+        <>
+          <Form.Select name='hinge' onChange={handleInputChange}>
+            <option value='CR3D62-3'>CalRoyal CR3D562 {`(3)`}</option>
+            <option value='540 3D-3'>Tectus 540 3D {`(3)`}</option>
+            <option value='butt-4'>4x4 butt hinge {`(4)`}</option>
+          </Form.Select>
+        </>
+      )
+    }
+  if(data.height > 96 && data.height <= 120){
+    return(
+      <>
+        <Form.Select name='hinge' onChange={handleInputChange}>
+          <option value='CR3D62-4'>CalRoyal CR3D562 {`(4)`}</option>
+          <option value='540 3D-4'>Tectus 540 3D {`(4)`}</option>
+          <option value='butt-4'>4x4 butt hinge {`(4)`}</option>
+        </Form.Select>
+      </>
+    )
+  }
 }
+
 
 export default Conditionals;
